89f8a107e13b528d4453d83bf8b84b24a6679370
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2026-03-01 23:09:02 +0500

    Dynamic Cards

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1. Fetch the JSON data
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    // 2. Get the container where cards will go
+    const grid = document.getElementById('extensions-grid');
+    
+    // 3. Loop through each extension and create HTML
+    data.forEach(extension => {
+      // Create the card HTML here
+      const card = document.createElement('div');
+      card.classList.add('card-here')
+
+      card.innerHTML = `
+        <div class="flex gap-5">
+                <img src="${extension.logo}" alt="devlens" class="self-start">
+                <div class="flex flex-col gap-2">
+                  <h2 class="text-white font-bold text-lg">${extension.name}</h2>
+                  <p class="paragraph-color">${extension.description}</p>
+                </div>
+              </div>
+              <div>
+                <button class="remove-button">Remove</button>
+              </div>
+      `
+
+      grid.appendChild(card);
+    });
+  });
+
