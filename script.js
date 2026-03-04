58cb4992ce3f23edfa4c5102d8559c71ce33281b
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2026-03-04 23:47:41 +0500

    Updated

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-// 1. Fetch the JSON data
-fetch("data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    // 2. Get the container where cards will go
-    const grid = document.getElementById("extensions-grid");
+document.addEventListener("DOMContentLoaded", function () {
+  const allButton = document.getElementById("all");
+  const activeButton = document.getElementById("active");
+  const inactive = document.getElementById("inactive");
 
-    // 3. Loop through each extension and create HTML
-    data.forEach((extension) => {
-      // Create the card HTML here
-      const card = document.createElement("div");
-      card.classList.add("card-here");
+  console.log("Active button is "+ activeButton.textContent)
 
-      card.innerHTML = `
+  // 1. Fetch the JSON data
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // 2. Get the container where cards will go
+      const grid = document.getElementById("extensions-grid");
+
+      activeButton.addEventListener("mouseenter", function () {
+        console.log("Hello hovered")
+        data.forEach((extension) => {
+          // Create the card HTML here
+          const card = document.createElement("div");
+          card.classList.add("card-here");
+          console.log(extension.isActive)
+          if (extension.isActive) {
+            card.innerHTML = `
         <div class="flex gap-5">
                 <img src="${extension.logo}" alt="devlens" class="self-start">
                 <div class="flex flex-col gap-2">
@@ -23,12 +32,43 @@ fetch("data.json")
                 <button class="remove-button">Remove</button>
                 <!-- Rounded switch -->
                 <label class="switch">
-                  <input type="checkbox" ${extension.isActive? "checked" : ""}>
+                  <input type="checkbox" ${extension.isActive ? "checked" : ""}>
+                  <span class="slider round"></span>
+                </label>
+              </div>
+      `;
+            grid.appendChild(card);
+          }
+          else{
+            rem
+          }
+        });
+      });
+      // 3. Loop through each extension and create HTML
+      data.forEach((extension) => {
+        // Create the card HTML here
+        const card = document.createElement("div");
+        card.classList.add("card-here");
+
+        card.innerHTML = `
+        <div class="flex gap-5">
+                <img src="${extension.logo}" alt="devlens" class="self-start">
+                <div class="flex flex-col gap-2">
+                  <h2 class="text-white font-bold text-lg">${extension.name}</h2>
+                  <p class="paragraph-color">${extension.description}</p>
+                </div>
+              </div>
+              <div class="flex justify-between item-center">
+                <button class="remove-button">Remove</button>
+                <!-- Rounded switch -->
+                <label class="switch">
+                  <input type="checkbox" ${extension.isActive ? "checked" : ""}>
                   <span class="slider round"></span>
                 </label>
               </div>
       `;
 
-      grid.appendChild(card);
+        grid.appendChild(card);
+      });
     });
-  });
+});
